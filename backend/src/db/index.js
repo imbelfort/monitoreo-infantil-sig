74750c50b6ecdc5712295faa3206510350d58841
imbelfort <belfort.ishmael25@outlook.com>
2025-12-06 11:05:04 -0400

    Update index.js

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -1,12 +1,23 @@
 const { Pool } = require('pg');
 
-const pool = new Pool({
-  host: process.env.PGHOST,
-  port: process.env.PGPORT,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE
-});
+const connectionString = process.env.POSTGRES_URL || process.env.DATABASE_URL;
+
+const pool = new Pool(
+  connectionString
+    ? {
+      connectionString,
+      ssl: {
+        rejectUnauthorized: false
+      }
+    }
+    : {
+      host: process.env.PGHOST,
+      port: process.env.PGPORT,
+      user: process.env.PGUSER,
+      password: process.env.PGPASSWORD,
+      database: process.env.PGDATABASE
+    }
+);
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
